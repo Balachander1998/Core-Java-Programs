02daec7ec1f12dbf427c41a4b6a10a7b8efbc488
Balachander1998 <41260222+Balachander1998@users.noreply.github.com>
2018-10-09 05:48:44 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+public class Addition
+    {
+	public static void main(String args[])
+	{
+		int num1,num2,sum;
+	    	Scanner s=new Scanner(System.in);
+	    	num1=s.nextInt();
+	    	num2=s.nextInt();
+	    	sum=num1+num2;
+	    	System.out.println("Sum of two numbers is "+sum);
+	   
+	    
+	}
+}
